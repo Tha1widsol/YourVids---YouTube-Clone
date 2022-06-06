4765225df2131dbea4cf218a0664cd42907e3b7b
Kelvin <laikelvin8@gmail.com>
2022-06-06 22:13:20 +0100

    Added RegisterPage.tsx

---
+++ frontend/src/components/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function RegisterPage() {
+  return (
+    <div style = {{textAlign: 'center'}}>
+      <h1><u>Register</u></h1>
+      <form>
+        <label><p>Username:</p></label>
+        <input placeholder = 'Username...'/>
+
+        <label><p>Password:</p></label>
+        <input type = 'password' placeholder = 'Password...'/>
+
+        <label><p>Confirm password:</p></label>
+        <input type = 'password' placeholder = 'Confirm Password...'/>
+
+      </form>
+      <button>Submit</button>
+
+    </div>
+  )
+}
