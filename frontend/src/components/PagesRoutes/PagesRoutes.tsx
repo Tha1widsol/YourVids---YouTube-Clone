8bb80a5c6c71000583e619086de7965b808adc27
Kelvin <laikelvin8@gmail.com>
2022-06-06 22:13:07 +0100

    Added PagesRoutes.tsx

---
+++ frontend/src/components/PagesRoutes/PagesRoutes.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import HomePage from '../HomePage/HomePage'
+import RegisterPage from '../RegisterPage/RegisterPage'
+import LoginPage from '../LoginPage/LoginPage'
+
+export default function PagesRoutes() {
+  return (
+    <div>
+        <Routes>
+            <Route path = '/' element = {<HomePage/>}/>
+            <Route path = '/home' element = {<HomePage/>}/>
+            <Route path = '/register' element = {<RegisterPage/>}/>
+            <Route path = '/login' element = {<LoginPage/>}/>
+
+        </Routes>
+    </div>
+  )
+}
