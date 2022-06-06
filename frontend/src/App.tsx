74531fe1226182addae299ad89f14569739075c9
Kelvin <laikelvin8@gmail.com>
2022-06-06 22:12:01 +0100

    Added Navbar + PagesRoutes component tags to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import PagesRoutes from './components/PagesRoutes/PagesRoutes';
+import axios from 'axios'
 
 function App() {
+  axios.get('api/hello').then(response => {
+    console.log(response.data)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {{width: '95%', margin: '0 auto'}}>
+      <Navbar/>
+      <PagesRoutes/>
     </div>
   );
 }
