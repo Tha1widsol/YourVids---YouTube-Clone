dba8f1ce77e782f63513b25e0dbfdb6996144d29
Kelvin <laikelvin8@gmail.com>
2022-06-13 22:12:19 +0100

    Added username + password states

--- frontend/src/components/RegisterPage/RegisterPage.tsx
+++ frontend/src/components/RegisterPage/RegisterPage.tsx
@@ -1,20 +1,48 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from 'axios'
 
 export default function RegisterPage() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  function handleSubmit(){
+    const requestOptions = { 
+      headers:{
+        'Content-Type':'application/json',
+        'Accept':'application/json'
+      }
+  }
+
+    fetch('api/register', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers:{
+        'Content-Type':'application/json',
+        'Accept':'application/json'
+      }
+
+    })
+    .then(response => {
+      console.log('User created')
+    })
+  }
+
   return (
     <div style = {{textAlign: 'center'}}>
       <h1><u>Register</u></h1>
       <form>
         <label><p>Username:</p></label>
-        <input placeholder = 'Username...'/>
+        <input placeholder = 'Username...' value = {username} onChange = {e => setUsername(e.target.value)}/>
 
         <label><p>Password:</p></label>
-        <input type = 'password' placeholder = 'Password...'/>
+        <input type = 'password' value = {password} onChange = {e => setPassword(e.target.value)} placeholder = 'Password...'/>
 
         <label><p>Confirm password:</p></label>
-        <input type = 'password' placeholder = 'Confirm Password...'/>
+        <input type = 'password' value = {confirmPassword} onChange = {e => setConfirmPassword(e.target.value)} placeholder = 'Confirm Password...'/>
       </form>
-      <button type = 'submit'>Submit</button>
+    <button type = 'submit' onClick = {handleSubmit}>Submit</button>
+      
 
     </div>
   )
