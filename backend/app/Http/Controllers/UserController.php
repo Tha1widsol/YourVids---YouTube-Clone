8971bd1571f0babcb85e519d9b3051e3a39c9877
Kelvin <laikelvin8@gmail.com>
2022-06-17 22:30:18 +0100

    Hashed user password

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function createUser(Request $request){
+    public function register(Request $request){
         $user = new User;
         $user->username = $request->username;
-        $user->password = $request->password;
+        $user->password = Hash::make($request->password);
         $user->save();
         return $request-> input();
     }
