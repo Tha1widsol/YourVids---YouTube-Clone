67d65b65fe4ac5b68df83d503d1c72da7826a009
Kelvin <laikelvin8@gmail.com>
2022-06-18 22:32:49 +0100

    Added login and getUser functions

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Synfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
@@ -13,6 +15,32 @@ class UserController extends Controller
         $user->username = $request->username;
         $user->password = Hash::make($request->password);
         $user->save();
+        Auth::login($user);
         return $request-> input();
+    }
+
+    public function login(Request $request){
+        if (!Auth::attempt(['username' => $request->input('username'), 'password' => $request->input('password')])){
+            return response(['Message' => 'Invalid credentials']);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('token')->plainTextToken;
+        $cookie = cookie('jwt',$token, 68 * 24);
+
+        return response([
+            'message' => 'Success'
+        ]) -> withCookie($cookie);
+    }
+
+    public function getUser(){
+        return Auth::user();
+    }
+
+    public function logout(Request $request){
+        $request->user()->tokens()->delete();
+        return response([
+            'message' => 'Logged out'
+        ]);
     }
 }
