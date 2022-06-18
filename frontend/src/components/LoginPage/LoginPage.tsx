b2faa9f2e83c8f042a278538635f6b67b6ed8c91
Kelvin <laikelvin8@gmail.com>
2022-06-18 22:34:15 +0100

    Added functionality to login

--- frontend/src/components/LoginPage/LoginPage.tsx
+++ frontend/src/components/LoginPage/LoginPage.tsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 
 export default function LoginPage() {
+  const [username,setUsername] = useState('')
+  const [password,setPassword] = useState('')
+
+function handleSubmit(){
+  const requestOptions = { 
+    headers:{'Content-Type':'application/json', 'Accept':'application/json'}
+}
+
+  axios.post('api/login',JSON.stringify({username: username, password: password}), requestOptions)
+  .then(response => {
+    console.log(response.data)
+  })
+}
+
+  function handleLogout(){
+    axios.post('/api/logout')
+  }
+
   return (
     <div style = {{textAlign: 'center'}}>
        <h1><u>Login</u></h1>
       <form>
         <label><p>Username:</p></label>
-        <input placeholder = 'Username...'/>
+        <input onChange = {e => setUsername(e.target.value)} value = {username} placeholder = 'Username...'/>
 
         <label><p>Password:</p></label>
-        <input type = 'password' placeholder = 'Password...'/>
+        <input onChange = {e => setPassword(e.target.value)} value = {password} type = 'password' placeholder = 'Password...'/>
 
       </form>
-      <button>Submit</button>
+      <button onClick = {handleSubmit}>Submit</button>
     </div>
   )
 }
