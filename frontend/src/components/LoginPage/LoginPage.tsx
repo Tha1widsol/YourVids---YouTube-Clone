f4d9641f4bbc9cd62d7377999f8b6375e19d90cd
Kelvin <laikelvin8@gmail.com>
2022-07-08 17:37:35 +0100

    Refreshed after login

--- frontend/src/components/LoginPage/LoginPage.tsx
+++ frontend/src/components/LoginPage/LoginPage.tsx
@@ -4,19 +4,25 @@ import {login} from '../../features/Auth/auth';
 import axios from 'axios'
 
 export default function LoginPage() {
-  const [username,setUsername] = useState('')
-  const [password,setPassword] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   const dispatch = useAppDispatch()
 
   function handleSubmit(){
     const requestOptions = { 
       headers:{'Content-Type':'application/json', 'Accept':'application/json'}
     }
+
    axios.post('api/login',JSON.stringify({username: username, password: password}), requestOptions)
    .then(response => {
-    if (response.status === 200) {
+
+    if (response.data.message === 'success'){
       dispatch(login())
+      window.location.reload()
     }
+
+    else console.log(response.data.message)
+
     
    })
 }
