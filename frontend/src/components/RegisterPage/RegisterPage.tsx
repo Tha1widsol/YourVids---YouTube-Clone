2ebab17dc8873bb33385e37baccd19e98813752e
Kelvin <laikelvin8@gmail.com>
2022-07-08 17:37:52 +0100

    Validated register form

--- frontend/src/components/RegisterPage/RegisterPage.tsx
+++ frontend/src/components/RegisterPage/RegisterPage.tsx
@@ -1,28 +1,61 @@
 import React,{useState} from 'react'
+import {useAppDispatch} from '../../app/hooks'
+import {login} from '../../features/Auth/auth'
+import Errors from '../Messages/Errors'
+import axios from 'axios'
 
 export default function RegisterPage() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
+  const [errors, setErrors] = useState<Array<string>>([])
+  const dispatch = useAppDispatch()
 
-  function handleSubmit(){
-    fetch('api/register', {
-      method: 'POST',
-      body: JSON.stringify({username, password}),
-      headers:{
-        'Content-Type':'application/json',
-        'Accept':'application/json'
-      }
+  const validateForm = () => {
+    let isValid = true
+    const errorMsgs: Array<string> = []
 
-    })
+    if (username === '' || password === '' || confirmPassword === ''){
+      errorMsgs.push('Fields are required')
+      isValid = false
+    }
+
+    if (password !== confirmPassword){
+      errorMsgs.push("Passwords don't match")
+      isValid = false
+    }
+
+    if (!isValid) setErrors(errorMsgs)
+    return isValid
+  }
+
+  function handleSubmit(e: React.SyntheticEvent){
+    e.preventDefault()
+
+    if (!validateForm()) return
+
+    const requestOptions = { 
+      headers:{'Content-Type':'application/json', 'Accept':'application/json'}
+    }
+
+    axios.post('api/register', JSON.stringify({username: username, password: password}), requestOptions)
     .then(response => {
-      console.log('User created')
+      if (response.data.message === 'success'){
+          dispatch(login())
+          window.location.reload()
+        }
+      })
+
+    .catch(() => {
+      setErrors(['Username already exists'])
     })
+     
   }
 
   return (
     <div style = {{textAlign: 'center'}}>
       <h1><u>Register</u></h1>
+      <Errors errors = {errors} />
       <form>
         <label><p>Username:</p></label>
         <input placeholder = 'Username...' value = {username} onChange = {e => setUsername(e.target.value)}/>
