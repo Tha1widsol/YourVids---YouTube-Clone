beb7780ae034859682868d8845a688d77bda92db
Kelvin <laikelvin8@gmail.com>
2022-07-12 04:37:50 +0100

    Made migrations to db

---
+++ backend/database/migrations/2022_07_12_025037_channel.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channels', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name');
+            $table->bigInteger('subscribers')->default(0);
+            $table->bigInteger('views')->default(0);
+            $table->string('logo')->nullable();
+            $table->string('banner')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
