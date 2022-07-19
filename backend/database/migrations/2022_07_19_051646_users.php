807e94ebabb767c75e6ebc2a8e0922df195e7790
Kelvin <laikelvin8@gmail.com>
2022-07-19 15:51:42 +0100

    Made migrations to db

--- backend/database/migrations/2022_07_19_051646_users.php
+++ backend/database/migrations/2022_07_19_051646_users.php
@@ -13,7 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique();;
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
     }
 
     /**
