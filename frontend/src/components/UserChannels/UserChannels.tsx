03ae283dcb585745214fc67eed8ede073b8c0ed7
Kelvin <laikelvin8@gmail.com>
2022-07-24 10:05:59 +0100

    Added link to channel page from channel name

--- frontend/src/components/UserChannels/UserChannels.tsx
+++ frontend/src/components/UserChannels/UserChannels.tsx
@@ -1,4 +1,5 @@
 import React,{useState, useRef} from 'react'
+import {Link} from 'react-router-dom'
 import Popup from '../Popup/Popup'
 import Errors from '../Messages/Errors'
 import {FileProps} from '../../app/types/files'
@@ -83,10 +84,10 @@ export default function UserChannels() {
         <button onClick = {() => setPopup(prev => {return{...prev, create: true}})}>Add channel</button>
 
         <div style = {{display: 'flex', flexDirection: 'column', gap: '10px'}}>
-        {channels.data?.map((channel: any, index: number) => {
+        {channels.data?.map((channel, index) => {
           return(
               <div className = 'channelContainer' key = {index}>
-                <h2>{channel.name}</h2>
+                <Link to = {`/channel/${channel.id}`}><h2 className = 'link disable-select'>{channel.name}</h2></Link>
                 <p style = {{fontSize: 'small', color: 'gray'}}>Creation date - {channel.created_at}</p>
                 <p>Subscribers - {channel.subscribers}</p>
                 <p>Total views - {channel.views}</p>
