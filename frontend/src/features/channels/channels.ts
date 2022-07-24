30c5062461d5b000483e2cd084542477f569a7ac
Kelvin <laikelvin8@gmail.com>
2022-07-24 10:06:28 +0100

    Added channelApi reducer

--- frontend/src/features/channels/channels.ts
+++ frontend/src/features/channels/channels.ts
@@ -1,8 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
 interface ChannelProps{
     values:
+    {
+    id: number
+    name: string
+    description: string
+    subscribers: number
+    views: number
+    logo: string
+    banner: string
+    created_at: string
+    }
+}
+
+interface ChannelsProps{
+    values:
     [{
+    id: number
     name: string
     description: string
     subscribers: number
@@ -17,10 +31,21 @@ export const channelsApi = createApi({
     reducerPath: 'channelsAPI',
     baseQuery: fetchBaseQuery({baseUrl: '/api/'}),
     endpoints: (builder) => ({
-        getUserChannels: builder.query<ChannelProps['values'], null>({
+        getUserChannels: builder.query<ChannelsProps['values'], null>({
             query: () => 'userChannels'
         })
     })
 })
 
+export const channelApi = createApi({
+    reducerPath: 'channelAPI',
+    baseQuery: fetchBaseQuery({baseUrl: '/api/'}),
+    endpoints: (builder) => ({
+        getChannel: builder.query<ChannelProps['values'], string>({
+            query: (id) => `getChannel?id=${id}`
+        })
+    })
+})
+
 export const {useGetUserChannelsQuery} = channelsApi
+export const {useGetChannelQuery} = channelApi
