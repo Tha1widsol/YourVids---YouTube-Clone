337e6e79ef6079764f81a7fba5b3180da566a011
Kelvin <laikelvin8@gmail.com>
2022-07-27 18:17:39 +0100

    Displayed channel data on channel page

--- frontend/src/components/ChannelPage/ChannelPage.tsx
+++ frontend/src/components/ChannelPage/ChannelPage.tsx
@@ -8,7 +8,27 @@ export default function ChannelPage() {
     
   return (
     <div>
+      
+      <div className = 'center'>
+      {channel.isSuccess ? 
+      <>
         <h1>{channel.data?.name}</h1>
+        <p>{channel.data?.description}</p>
+        <p style = {{color: 'gray'}}>{channel.data?.subscribers} subscribers</p>
+        <button type = 'button'>Upload video</button>
+        <hr className = 'mt-0-mb-4'/>
+      </>
+      : channel.isLoading ? 
+        <>
+          <p>Loading...</p>
+        </>
+      : <>
+        <h1>Channel doesn't exist</h1>
+        </>}
+         
+
+      </div>
+       
     </div>
   )
 }
