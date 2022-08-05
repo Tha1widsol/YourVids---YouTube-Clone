6e0ab6f409fc1e010b6037c6fb6209476968e09f
Kelvin <laikelvin8@gmail.com>
2022-08-05 20:47:22 +0100

    Added VideoController.php

---
+++ backend/app/Http/Controllers/VideoController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth, DB;
+use App\Models\Channel, Video;
+
+class VideoController extends Controller
+{
+    public function createVideo(Request $request){
+        $lookup_url_kwarg = 'id';
+        $channel_id = $request->$lookup_url_kwarg;
+        $video = new Video;
+
+        $video->title = $request->title;
+        $video->description = $request->description;
+        $video->category = $request->category;
+
+        if ($request->hasFile('thumbnail')){
+            $thumbnail = $request->file('thumbnail');
+            $thumbnailName = $thumbnail->getClientOriginalName();
+            $finalName = date('His') . $thumbnailName;
+            $pathName = $thumbnail->storeAs('thumbnails/', $finalName, 'public');
+            $channel->thumbnail = $pathName;
+        }
+
+        if ($request->hasFile('video')){
+            $file = $request->file('video');
+            $fileName = $file->getClientOriginalName();
+            $finalName = date('His') . $fileName;
+            $videoName = $file->storeAs('videos/', $finalName, 'public');
+            $channel->pathName = $videoName;
+        }
+
+        $video->channel_id =  intval($channel_id);
+        $video->save();
+
+
+    }
+}
