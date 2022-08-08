2cd55f8e9e6ab8d8e0af73cc32c0baad5053ca5b
Kelvin <laikelvin8@gmail.com>
2022-08-08 11:54:43 +0100

    Added dropdown menu to navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {NavLink} from 'react-router-dom';
 import './css/Navbar.css';
 import {useAppSelector, useAppDispatch} from '../../app/hooks';
 import {logout} from '../../features/Auth/auth';
+import {useGetUserChannelsQuery} from '../../features/channels/channels';
 import axios from 'axios'
 
 export default function Navbar() {
   const user = useAppSelector(state => state.user)
+  const [dropdown, setDropdown] = useState({menu: false, channels: false})
+  const channels = useGetUserChannelsQuery(null)
   const dispatch = useAppDispatch()
 
   function handleLogout(){
@@ -22,10 +25,36 @@ export default function Navbar() {
         <NavLink to = '/home'>Home</NavLink>
         {user.isLoggedIn ?  
         <>
-        <NavLink to = {`/user/${user.values?.username}/channels`}>Your channels</NavLink>
+        <NavLink to = {`/user/${user.values?.username}/channels`}>Your channel</NavLink>
         <NavLink to = '/videos'>Your videos</NavLink>
         <NavLink to = '/upload'>Upload</NavLink>
-        <button onClick = {handleLogout}>Logout</button>
+        <div className = 'dropdown'>
+          <button id = 'navDropBtn' onClick={() => setDropdown({menu: !dropdown.menu, channels: false})}>My account</button>
+              <div className = 'dropdown-content'>
+                {dropdown.menu ? 
+                    <>
+                    <p><b>{user.values?.username}</b></p>
+                    <button id = 'navDropBtn' onClick = {() => setDropdown({menu: false, channels: true})}>Switch channel</button>
+                    <NavLink to = '/videos'>My Videos</NavLink>
+                    <NavLink to = '/' onClick = {handleLogout}>Logout</NavLink>
+                    </>
+                    : dropdown.channels ?
+                    <>
+                    <div style = {{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                      <p>Channels:</p>
+                      <button id = 'navDropBtn' className = 'arrow' onClick = {() => setDropdown({menu: true, channels: false})}>Return</button>
+                    </div>
+                    {channels.data?.map((channel, index) => {
+                      return (
+                        <div key = {index}>
+                          <button id = 'navDropBtn'>{channel.name} <p style = {{fontSize: 'small', color: 'darkgray'}}>{channel.subscribers} subscribers</p></button>
+                        </div>
+                      )
+                    })}
+                    </>
+                    : null}
+              </div>
+        </div>
         </>
         :
         <>
