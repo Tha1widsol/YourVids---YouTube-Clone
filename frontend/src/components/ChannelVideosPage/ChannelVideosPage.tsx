2baeacea4b76461fd58f9d168ad4ab25ad216b10
Kelvin <laikelvin8@gmail.com>
2022-08-25 10:36:09 +0100

    Added channel videos in ChannelVideosPage.tsx

--- frontend/src/components/ChannelVideosPage/ChannelVideosPage.tsx
+++ frontend/src/components/ChannelVideosPage/ChannelVideosPage.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useGetCurrentChannelQuery } from '../../features/channels/currentChannel'
 import { useGetChannelVideosQuery } from '../../features/videos/channelVideos'
+import './css/ChannelVideosPage.css'
 
 export default function ChannelVideosPage() {
   const currentChannel = useGetCurrentChannelQuery(null)
@@ -10,7 +11,23 @@ export default function ChannelVideosPage() {
   return (
     <div>
       <h2>Your videos:</h2>
-
+      <section className = 'videosContainer'>
+        {videos.data?.map((video, index) =>{
+          return (
+            <Link to = {`/video/${video.id}`}>
+              <div className = 'videoContainer' key = {index}>
+                  <img className = 'thumbnail' src = {`/storage/${video.thumbnail || video.pathName}`} alt = ''/>
+                  <b><p>{video.title}</p></b>
+                  <p className = 'smallGray'>
+                    {video.views} views
+                    - {video.created_at.slice(0, 10)}
+                    </p>
+                  
+              </div>
+           </Link>
+          )
+        })}
+      </section>
     </div>
   )
 }
