7c3b3947f66145ee3927847653662ab7aeb86bb7
Kelvin <laikelvin8@gmail.com>
2022-08-25 22:00:51 +0100

    Finished building channel page

--- frontend/src/components/ChannelPage/ChannelPage.tsx
+++ frontend/src/components/ChannelPage/ChannelPage.tsx
@@ -1,36 +1,57 @@
 import React from 'react'
 import {useGetChannelQuery} from '../../features/channels/channel'
+import { useGetCurrentChannelQuery } from '../../features/channels/currentChannel'
 import {Link, useParams} from 'react-router-dom'
+import ChannelVideos from '../ChannelVideos/ChannelVideos'
+import './css/ChannelPage.css'
 
 export default function ChannelPage() {
     const {channelID} = useParams()
+    const currentChannel = useGetCurrentChannelQuery(null)
     const channel = useGetChannelQuery(channelID || '')
     
   return (
     <div>
-      
-      <div className = 'center'>
       {channel.isSuccess ? 
       <>
         {channel.data?.banner ? <img className = 'banner' src = {`/storage/${channel.data?.banner}`} alt = ''/> : null}
-        <h1>{channel.data?.name}</h1>
-        {channel.data?.logo ? <img className = 'logo' src = {`/storage/${channel.data?.logo}`} alt = ''/> : null}
-        <p>{channel.data?.description}</p>
-        <p style = {{color: 'gray'}}>{channel.data?.subscribers} subscribers</p>
+       <header className = 'channelHeader'>
+        <div style = {{display: 'flex', gap: '20px'}}>
+           {channel.data?.logo ? <img className = 'logo' src = {`/storage/${channel.data?.logo}`} alt = ''/> : null}
+           <div>
+              <p style = {{fontSize: 'larger'}}>{channel.data?.name}</p>
+              <p style = {{color: 'gray'}}>{channel.data?.subscribers} subscribers</p>
+           </div>
+        </div>
+         
+         <div>
+            {channelID != currentChannel.data?.id ? 
+            <>
+            <button>Subscribe</button> 
+            <button>Notify</button>
+            </>
+            : 
+            <>
+            <button>Customise channel</button> 
+            <Link to = '/videos'><button>Manage videos</button></Link>
+           </>}
+        
+         </div>
+
+       </header>
       
-        <Link to = '/upload'><button type = 'button'>Upload video</button></Link>
         <hr className = 'mt-0-mb-4'/>
+        <p>Videos:</p>
+        <ChannelVideos id = {channel.data?.id || ''}/>
       </>
       : channel.isLoading ? 
-        <>
+      <>
           <p>Loading...</p>
         </>
       : <>
-        <h1>Channel doesn't exist</h1>
+        <h2>Channel doesn't exist...</h2>
         </>}
-         
 
-      </div>
        
     </div>
   )
