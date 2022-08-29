b5d2a43b0ad5ccf6c5f7319e97e090d3e9affafc
Kelvin <laikelvin8@gmail.com>
2022-08-29 16:53:29 +0100

    Added condition for video upload progress

--- frontend/src/components/ChannelVideos/ChannelVideos.tsx
+++ frontend/src/components/ChannelVideos/ChannelVideos.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useAppSelector } from '../../app/hooks'
 import { useGetChannelVideosQuery } from '../../features/videos/channelVideos'
 import './css/ChannelVideos.css'
 
 export default function ChannelVideos({id}: {id: string | number}) {
   const videos = useGetChannelVideosQuery(id)
+  const videoProgress = useAppSelector(state => state.videoProgress.value)
   
   return videos.isSuccess ? (
     <>
        {videos.data?.length ? 
-            <section className = 'videosContainer'>
+       <>
+          <section className = 'videosContainer'>
                 {videos.data?.map((video, index) =>{
                 return (
                     <Link to = {`/video/${video.id}`} key = {index}>
-                        <div className = 'videoContainer' >
+                        <div className = 'videoContainer'>
                             <div className = 'thumbnailContainer'>
                                <img className = 'thumbnail' src = {`/storage/${video.thumbnail || video.pathName}`} alt = ''/>
                                <p className = 'duration'>{video.length}</p>
@@ -27,9 +30,17 @@ export default function ChannelVideos({id}: {id: string | number}) {
                     </Link>
                 )
                 })}
+                {videoProgress > 0 ? 
+                    <div className = 'thumbnail'>
+                        <p>Progress - {videoProgress}</p>
+                    </div>
+                : null}
+              
             </section>
+        </>
         :
         <>
+      
         <p>No videos posted...</p>
         <Link to = '/upload'><button>Upload</button></Link>
         </>}
