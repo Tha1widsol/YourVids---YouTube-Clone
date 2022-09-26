5f66d33cc6ddd0142b0aa1f9bb52fc3fe7fedcd7
Kelvin <laikelvin8@gmail.com>
2022-09-26 21:31:54 +0100

    Added Playlist tables and models

---
+++ backend/app/Models/Playlist.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Playlist extends Model
+{
+    use HasFactory;
+    protected $table = 'playlists';
+
+    public function videos() {
+        return $this->belongsToMany('App\Models\Video', 'playlist_videos', 'playlist_id', 'video_id')->withTimestamps();
+    }
+
+}
