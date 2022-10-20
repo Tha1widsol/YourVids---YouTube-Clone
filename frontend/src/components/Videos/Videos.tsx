3862425fb1b4cb72e1dba3df9bda3d185638fa63
Kelvin <laikelvin8@gmail.com>
2022-10-20 17:06:36 +0100

    Added playlist popup

--- frontend/src/components/Videos/Videos.tsx
+++ frontend/src/components/Videos/Videos.tsx
@@ -1,24 +1,59 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { VideosProps } from '../../features/videos/types/VideoProps'
-import { useAppSelector } from '../../app/hooks'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import ReactPlayer from 'react-player'
 import KebabMenu from '../KebabMenu/KebabMenu'
+import Popup from '../Popup/Popup'
+import { fetchPlaylists } from '../../features/playlists/playlists'
 import './css/Videos.css'
 
 export default function Videos({videos, isRow = true, isOwnVideos = false}: {videos: VideosProps['values'] | undefined, isRow?: boolean, isOwnVideos?: boolean}) {
   const videoProgress = useAppSelector(state => state.videoProgress.value)
   const [dropdown, setDropdown] = useState<number | null>(null)
+  const [popup, setPopup] = useState({playlist: false})
+  const dispatch = useAppDispatch()
+  const playlists = useAppSelector(state => state.playlists)
+  const currentChannelID = useAppSelector(state => state.currentChannel.values?.id)
+  const user = useAppSelector(state => state.user)
+  const [chosenVideo, setChosenVideo] =  useState(0)
+
+  useEffect(() => {
+    if (!user.isLoggedIn) return
+    dispatch(fetchPlaylists(currentChannelID))
+  },[dispatch])
+
+  function handlePlaylistPopup(videoID: number){
+    setPopup(prev => {return{...prev, playlist: true}})
+    setDropdown(null)
+    setChosenVideo(videoID)
+  }
+
   
   return videos ? (
     <>
+        <Popup trigger = {popup.playlist} switchOff = {() => setPopup(prev => {return{...prev, playlist: false}})}>
+           {playlists.values?.map((playlist, index) => {
+            return (
+                <div key = {index}>
+                    <h2>Add to playlist:</h2>
+                    <hr className = 'mt-0-mb-4'/>
+                    <div className = 'row' style = {{alignItems: 'center', gap: '10px'}}>
+                         <p>{playlist.title}</p>
+                         <input type = 'checkbox'/>
+                    </div>
+                   
+                </div>
+            )
+           })}
+        </Popup>
        {videos?.length ? 
        <>
           <section className = {`videosContainer ${isRow ? 'row' : 'col'} `}>
                 {videos?.map((video, index) =>{
                 return (
-                    <div className = 'videoContainer'>
-                        <Link to = {`/video/${video.id}`} key = {index}>
+                    <div className = 'videoContainer' key = {index}>
+                        <Link to = {`/video/${video.id}`} >
                             <div className = 'thumbnailContainer'>
                             <ReactPlayer width = '200px' height = '100px' url = {video.pathName ? `/storage/${video.pathName}`: ''} light = {video.thumbnail ? `/storage/${video.thumbnail}` : false} playIcon = {<></>}/>
                             <p className = 'duration'>{video.length}</p>
@@ -27,7 +62,7 @@ export default function Videos({videos, isRow = true, isOwnVideos = false}: {vid
                     
                         <KebabMenu current = {dropdown} many = {true} index = {index} switchOn = {() => setDropdown(index)} switchOff = {() => setDropdown(null)}>
                             <button className = 'dropdownBtn'>Save</button>
-                            <button className = 'dropdownBtn'>Add to playlist</button>
+                            <button className = 'dropdownBtn' onClick = {() => handlePlaylistPopup(video.id)}>Add to playlist</button>
                         </KebabMenu>
 
                         <Link to = {`/video/${video.id}`} key = {index}>
