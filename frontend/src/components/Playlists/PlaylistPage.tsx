b0d83b608d894d100e09d82c8a71b1a5d2398d2a
Kelvin <laikelvin8@gmail.com>
2022-10-31 23:44:24 +0000

    Added content to PlaylistPage.tsx

--- frontend/src/components/Playlists/PlaylistPage.tsx
+++ frontend/src/components/Playlists/PlaylistPage.tsx
@@ -1,21 +1,30 @@
 import React,{useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import { useAppSelector, useAppDispatch } from '../../app/hooks'
-import { fetchPlaylistVideos } from '../../features/videos/playlistVideos'
+import { fetchPlaylist } from '../../features/playlists/playlist'
 import Videos from '../Videos/Videos'
 
 export default function PlaylistPage() {
     const dispatch = useAppDispatch()
     const {playlistID} = useParams()
-    const videos = useAppSelector(state => state.playlistVideos)
+    const playlist = useAppSelector(state => state.playlist.values)
 
     useEffect(() => {
-      dispatch(fetchPlaylistVideos(playlistID))
+      dispatch(fetchPlaylist(playlistID))
     },[dispatch, playlistID])
 
   return (
     <div>
-        <Videos videos = {videos.values}/>
+        <div className = 'row' style = {{gap: '10px'}}>
+           <h2>{playlist.title}</h2>
+           <button>Edit</button>
+        </div>
+       
+         <p>{playlist.description}</p>
+         <p className = 'smallGray'>Videos: {playlist.videoCount}</p>
+         <p className = 'smallGray'>Visibilty: {playlist.visibility}</p>
+         <hr className = 'mt-0-mb-4'/>
+        <Videos videos = {playlist.videos}/>
     </div>
   )
 }
