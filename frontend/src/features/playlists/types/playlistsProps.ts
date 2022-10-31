4b78e15a241ef842858cd57b3363a1301dcbcad8
Kelvin <laikelvin8@gmail.com>
2022-10-31 23:43:29 +0000

    Added PlaylistProps interface

--- frontend/src/features/playlists/types/playlistsProps.ts
+++ frontend/src/features/playlists/types/playlistsProps.ts
@@ -1,4 +1,5 @@
 import { ChannelProps } from "../../channels/types/ChannelProps";
+import { VideosProps } from "../../videos/types/VideoProps";
 
 export interface PlaylistsProps{
     values: [{
@@ -10,6 +11,22 @@ export interface PlaylistsProps{
         videoCount: number
         visibility: 'Public' | 'Private' | 'Unlisted'
         created_at: string
+        videos: VideosProps['values']
     }],
+    status: '' | 'success' | 'loading' | 'rejected'
+}
+
+export interface PlaylistProps{
+    values: {
+        id: number
+        channel: ChannelProps['values']
+        title: string
+        description: string
+        views: number
+        videoCount: number
+        visibility: 'Public' | 'Private' | 'Unlisted'
+        created_at: string
+        videos: VideosProps['values']
+    },
     status: '' | 'success' | 'loading' | 'rejected'
 }
