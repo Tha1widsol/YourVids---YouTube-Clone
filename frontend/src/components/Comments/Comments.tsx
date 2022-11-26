fb1f211487bd33e7ea29755cf92eba61d7893c00
Kelvin <laikelvin8@gmail.com>
2022-11-26 00:17:35 +0000

    Added comments list in Comments component

--- frontend/src/components/Comments/Comments.tsx
+++ frontend/src/components/Comments/Comments.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { CommentsProps } from '../../features/comments/types/CommentProps'
+import './css/Comments.css'
 
-export default function Comments() {
+export default function Comments({comments}: {comments: CommentsProps['values']}) {
   return (
-    <div>
-        
-    </div>
+    <>
+        {comments.map((comment, index) => {
+          return (
+            <div className = 'comments' key = {index}>
+                <span className = 'row' style = {{gap: '20px'}}>
+                   {comment.channel?.logo ? <img className = 'logo' src = {`/storage/${comment.channel?.logo}`} alt = ''/> : <img className = 'logo' alt = ''/>}
+                   <div>
+                      <b style = {{fontWeight: 500}}>{comment.channel?.name}</b>
+                      <p>{comment.text}</p>
+                        <div className = 'row likesDislikes'>
+                          <i className = 'fa fa-thumbs-up'/>
+                          <p>{comment.likes}</p>
+                          <i className = 'fa fa-thumbs-down'/>
+                          <p>{comment.dislikes}</p>
+                          <button>Reply</button>
+                       </div>
+                   </div>
+      
+                </span>
+             
+             </div>
+          )
+        })}
+    </>
   )
 }
