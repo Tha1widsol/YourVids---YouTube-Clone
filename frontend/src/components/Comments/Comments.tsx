0da993e022eddac65d8b40e390455db283da6c1c
Kelvin <laikelvin8@gmail.com>
2022-12-01 23:47:13 +0000

    Finished handleSubmit reply function

--- frontend/src/components/Comments/Comments.tsx
+++ frontend/src/components/Comments/Comments.tsx
@@ -1,4 +1,5 @@
 import React,{useState} from 'react'
+import { useParams } from 'react-router-dom'
 import { CommentsProps } from '../../features/comments/types/CommentProps'
 import Popup from '../Popup/Popup'
 import { useAppSelector } from '../../app/hooks'
@@ -6,27 +7,40 @@ import './css/Comments.css'
 import axios from 'axios'
 
 export default function Comments({comments}: {comments: CommentsProps['values']}) {
-  const [popup, setPopup] = useState({reply: false, comment: {id: 0, channelID: 0, videoID: 0}})
+  const [popup, setPopup] = useState({reply: false, comment: {id: 0, channelID: 0}})
   const [reply, setReply] = useState('')
   const currentChannel = useAppSelector(state => state.currentChannel)
+  const {videoID} = useParams()
 
   function handleSubmit(e: React.SyntheticEvent){
+    e.preventDefault()
+  
     const requestOptions = {
-      headers: {'Content-Type': 'multipart/form-data'}
+      headers: {'Content-Type':'application/json', 'Accept':'application/json'}
     }
+    console.log(popup.comment)
+
     let form = new FormData()
     form.append('text', reply)
-    form.append('comment_id', String(popup.comment.id))
-    form.append('channel_id', String(currentChannel.values?.id))
-    form.append('replyToChannel_id', String(popup.comment.channelID))
-    form.append('video_id', String(popup.comment.videoID))
+    form.append('comment_id', String(popup.comment.id) || '')
+    form.append('channel_id', String(currentChannel.values.id) || '')
+    form.append('replyToChannel_id', String(popup.comment.channelID) || '')
+    form.append('video_id', videoID || '')
+
+    axios.post(`/api/postReply`,form, requestOptions)
+    .then(response => {
+      if (response.status === 200){
+        console.log(response.data)
+      }
+    })
     
   }
+
 
   return (
     <>
       <Popup trigger = {popup.reply} switchOff = {() => setPopup(prev => {return{...prev, reply: false}})} modalOn = {false}>
-        <form className = 'popupForm'>
+        <form className = 'popupForm' onSubmit = {handleSubmit}>
             <textarea placeholder = 'Reply to this comment...' value = {reply} onChange = {e => setReply(e.target.value)}/>
             <div className = 'row' style = {{justifyContent: 'space-evenly'}}>
               <button>Reply</button>
@@ -48,7 +62,7 @@ export default function Comments({comments}: {comments: CommentsProps['values']}
                           <p>{comment.likes}</p>
                           <i className = 'fa fa-thumbs-down'/>
                           <p>{comment.dislikes}</p>
-                          <button onClick = {() => setPopup(prev => {return{...prev, reply: true, commentID: comment.id, channelID: comment.channel.id, videoID: comment.videoID}})}>Reply</button>
+                          <button onClick = {() => setPopup(prev => ({...prev, reply: true, comment: {...prev, id: comment.id, channelID: comment.channel.id}}))}>Reply</button>
                        </div>
                    </div>
       
