c27ea08ae81b99682e8b86bae5331d31005e0b69
Kelvin <laikelvin8@gmail.com>
2022-12-31 23:20:14 +0000

    Modified useEffect hook to use dictionary for likes/dislikes

--- frontend/src/components/Comments/Comment.tsx
+++ frontend/src/components/Comments/Comment.tsx
@@ -1,135 +1,125 @@
 import React,{useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
-import { useAppSelector, useAppDispatch } from '../../app/hooks'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import { setCommentLikes, setCommentDislikes } from '../../features/comments/comments'
 import { CommentProps } from '../../features/comments/types/CommentProps'
 import './css/Comments.css'
 import axios from 'axios'
 
-export default function Comment({comment, parentChannelName = '', replyOn} : {comment: CommentProps, parentChannelName?: string, replyOn: () => void}) {
-    const dispatch = useAppDispatch()
-    const user = useAppSelector(state => state.user)
-    const [liked, setLiked] = useState(false)
-    const [disliked, setDisliked] = useState(false)
-
-    useEffect(() => {
-        if (user.isLoggedIn){
-                axios.get(`/api/checkLikedComment?id=${comment.id}`)
-                .then(response => {
-                    if (response.status === 200){
-                        const data = response.data
-            
-                        if (data.liked) {
-                            setLiked(true)
-                        }
-            
-                        if (data.disliked) {
-                            setDisliked(true)
-                        }
-                    }
-                   
+    export default function Comment({comment, likedDislikedComments, parentChannelName = '', replyOn} : {comment: CommentProps, likedDislikedComments: Record<number | string,{comment_id: number, liked: boolean, disliked: boolean}>, parentChannelName?: string, replyOn: () => void}) {
+        const dispatch = useAppDispatch()
+        const [liked, setLiked] = useState(false)
+        const [disliked, setDisliked] = useState(false)
+        const user = useAppSelector(state => state.user)
         
+        useEffect(() => {
+            if (user.isLoggedIn){
+                if (!(comment.id in likedDislikedComments)) return
+
+                if (likedDislikedComments[comment.id].liked){
+                    setLiked(true)
+                    return
+                }
+                setDisliked(true)
+            }
+        },[dispatch, likedDislikedComments, user.isLoggedIn, comment.id])
+    
+        function handleLikeComment(){
+            setLiked(!liked)
+            setDisliked(false)
+
+            if (!liked){
+                dispatch(setCommentLikes({
+                    root_id: comment.root_id,
+                    id: comment.id,
+                    value: comment.likes + 1
+                })) 
+                
+                axios.post(`/api/likeComment?id=${comment.id}`)
+                .then(response => {
+                    if (response.status !== 200){
+                        dispatch(setCommentLikes({
+                            root_id: comment.root_id,
+                            id: comment.id,
+                            value: comment.likes - 1
+                        })) 
+                    }
                 })
-        }
-      
-    },[dispatch])
 
-    function handleLikeComment(){
-        setLiked(!liked)
-        setDisliked(false)
+                if (disliked) dispatch(setCommentDislikes({
+                    root_id: comment.root_id,
+                    id: comment.id,
+                    value: comment.dislikes - 1
+                }))
 
-        if (!liked){
+                return
+            }
+
             dispatch(setCommentLikes({
                 root_id: comment.root_id,
                 id: comment.id,
-                value: comment.likes + 1
-            })) 
-            
-            axios.post(`/api/likeComment?id=${comment.id}`)
+                value: comment.likes - 1
+            }))
+
+            axios.delete(`/api/comment/removeLikeDislike?id=${comment.id}`)
             .then(response => {
                 if (response.status !== 200){
                     dispatch(setCommentLikes({
                         root_id: comment.root_id,
                         id: comment.id,
-                        value: comment.likes - 1
-                    })) 
+                        value: comment.likes + 1
+                    }))
                 }
             })
+        }
 
-            if (disliked) dispatch(setCommentDislikes({
+        function handleDislikeComment(){
+            setDisliked(!disliked)
+            setLiked(false)
+
+            if (!disliked){
+                dispatch(setCommentDislikes({
+                    root_id: comment.root_id,
+                    id: comment.id,
+                    value: comment.dislikes + 1
+                }))
+
+                axios.post(`/api/dislikeComment?id=${comment.id}`)
+                .then(response => {
+                    if (response.status !== 200){
+                        dispatch(setCommentDislikes({
+                            root_id: comment.root_id,
+                            id: comment.id,
+                            value: comment.dislikes - 1
+                        }))
+                    }
+                })
+
+                if (liked) dispatch(setCommentLikes({
+                    root_id: comment.root_id,
+                    id: comment.id,
+                    value: comment.likes - 1
+                }))
+                return
+            }
+
+            dispatch(setCommentDislikes({
                 root_id: comment.root_id,
                 id: comment.id,
                 value: comment.dislikes - 1
             }))
 
-            return
-        }
-
-        dispatch(setCommentLikes({
-            root_id: comment.root_id,
-            id: comment.id,
-            value: comment.likes - 1
-        }))
-
-        axios.delete(`/api/comment/removeLikeDislike?id=${comment.id}`)
-        .then(response => {
-            if (response.status !== 200){
-                dispatch(setCommentLikes({
-                    root_id: comment.root_id,
-                    id: comment.id,
-                    value: comment.likes + 1
-                }))
-            }
-        })
-    }
-
-    function handleDislikeComment(){
-        setDisliked(!disliked)
-        setLiked(false)
-
-        if (!disliked){
-            dispatch(setCommentDislikes({
-                root_id: comment.root_id,
-                id: comment.id,
-                value: comment.dislikes + 1
-            }))
-
-            axios.post(`/api/dislikeComment?id=${comment.id}`)
+            axios.delete(`/api/comment/removeLikeDislike?id=${comment.id}`)
             .then(response => {
                 if (response.status !== 200){
                     dispatch(setCommentDislikes({
                         root_id: comment.root_id,
                         id: comment.id,
-                        value: comment.dislikes - 1
+                        value: comment.dislikes + 1
                     }))
                 }
             })
-
-            if (liked) dispatch(setCommentLikes({
-                root_id: comment.root_id,
-                id: comment.id,
-                value: comment.likes - 1
-            }))
-            return
-        }
-
-        dispatch(setCommentDislikes({
-            root_id: comment.root_id,
-            id: comment.id,
-            value: comment.dislikes - 1
-        }))
-
-        axios.delete(`/api/comment/removeLikeDislike?id=${comment.id}`)
-        .then(response => {
-            if (response.status !== 200){
-                  dispatch(setCommentDislikes({
-                    root_id: comment.root_id,
-                    id: comment.id,
-                    value: comment.dislikes + 1
-                }))
-            }
-        })
-    } 
+        } 
 
   return (
         <span className = 'row' style = {{gap: '20px'}}>
